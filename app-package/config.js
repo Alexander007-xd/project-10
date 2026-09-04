d5b910425dbfbd5ddb8f943f3557f439c069fd61
Alexander007-xd <rifatsarkartuhin@gmail.com>
2026-09-04 20:47:25 +0600

    git add . git commit -m "Connect remote backend" git push

--- app-package/config.js
+++ app-package/config.js
@@ -1,3 +1,3 @@
 // Set this to the public HTTPS URL of the deployed Python backend.
 // Leave empty for local development on http://localhost:5000.
-window.MODEL_MATCH_API_URL = '';
+window.MODEL_MATCH_API_URL = 'https://project-10-e1v6.onrender.com';
